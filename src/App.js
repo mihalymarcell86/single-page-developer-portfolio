70d69ed67269c41ebdf1e18b2d88789afe520a4f
Marcell Mihály <mihalymarcell86@gmail.com>
2022-11-25 03:08:13 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      adamkeyes Nice to meet you! I'm Adam Keyes. Based in the UK, I'm a
+      front-end developer passionate about building accessible web apps that
+      users love. Contact me HTML 4 Years Experience CSS 4 Years Experience
+      JavaScript 4 Years Experience Accessibility 4 Years Experience React 3
+      Years Experience Sass 3 Years Experience Projects Contact me Design
+      portfolio HTML CSS View project View code E-learning landing page HTML CSS
+      View project View code Todo web app HTML CSS JavaScript View project View
+      code Entertainment web app HTML CSS JavaScript View project View code
+      Memory Game HTML CSS JavaScript View project View code Art gallery
+      showcase HTML CSS JavaScript View project View code Contact I would love
+      to hear about your project and how I could help. Please fill in the form,
+      and I’ll get back to you as soon as possible. Name Email Message Send
+      message adamkeyes
+    </>
   );
 }
 
